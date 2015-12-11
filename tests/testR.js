87b06a3c0070085de02c03c3691643adec8a7d65
salboaie <master1!23>
2015-12-11 18:48:37 +0200

    wip

--- tests/testR.js
+++ tests/testR.js
@@ -2,51 +2,56 @@
 
 var t = require("../lib/transformation.js");
 var assert = require("semantic-firewall").assert;
-var client = t.getRestClient();
+var client = t.createRestClient();
 
 var repository  = {};
 var counter = 0;
 
-var webServer = t.restAPI({
-    getEntity: {
-        method:'get',
-        params: ['entityId', 'token'],
-        path:'$entityId/$token',
-        code:function(entityId, token){
-            return repository[entityId];
-        }
-    },
-    createEntity: {
-        method: 'put',
-        params: ['token', '__body'],
-        path : 'token=$token',
-        code:function(token, __body){
-            counter++;
-            repository[counter] = __body;
-            return true;
-        }
-    },
-    updateEntity: {
-        method: 'post',
-        params: ['entityId', 'token', '__body'],
-        path : '$entityId/$token',
-        code:function(entityId, token){
-            repository[counter] = __body;
-            return true;
-        }
-    },
-    deleteEntity: {
-        method: 'post',
-        params: ['entityId', 'token'],
-        path : '$entityId/$token',
-        code:function(entityId, token){
-            delete repository[counter];
-        }
-    }
-});
 
-assert.steps("CRUD test",[ function(next) {
-            client.get("http://localhost/100/100", function (res, err) {
+assert.steps("CRUD test",[
+        function(next) {
+            var webServer = t.restAPI({
+                port:3000,
+                getEntity: {
+                    method:'get',
+                    params: ['entityId', 'token'],
+                    path:'$entityId/$token',
+                    code:function(entityId, token){
+                        return repository[entityId];
+                    }
+                },
+                createEntity: {
+                    method: 'put',
+                    params: ['token', '__body'],
+                    path : 'token=$token',
+                    code:function(token, __body){
+                        counter++;
+                        repository[counter] = __body;
+                        return true;
+                    }
+                },
+                updateEntity: {
+                    method: 'post',
+                    params: ['entityId', 'token', '__body'],
+                    path : '$entityId/$token',
+                    code:function(entityId, token){
+                        repository[counter] = __body;
+                        return true;
+                    }
+                },
+                deleteEntity: {
+                    method: 'post',
+                    params: ['entityId', 'token'],
+                    path : '$entityId/$token',
+                    code:function(entityId, token){
+                        delete repository[counter];
+                    }
+                }
+            });
+            next();
+        },
+        function(next) {
+            client.get("http://localhost:3000/100/100", function (res, err) {
                 assert.equal(err, null);
                 assert.equal(res, "undefined");
                 next();
